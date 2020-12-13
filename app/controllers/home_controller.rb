aef9b924f5b0a49dec8fa5dca569c6b0293c42a8
Godfrey Huang <{ID}+{username}@users.noreply.github.com>
2020-12-13 20:47:52 +0800

    update locale

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,11 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    if user_signed_in?
-      render_bookmarks
-    else
-      render "landing", layout: "landing"
-    end
+    # if user_signed_in?
+    render_bookmarks
+    # else
+    #   render "landing", layout: "landing"
+    # end
   end
 
   def about
